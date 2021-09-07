99a61476e946b3a8d4f258175fb4302b57f2c741
srisivas <64682294+srisivas@users.noreply.github.com>
2021-09-07 13:02:45 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,45 @@
+let songs = [
+    {
+        name: 'Vaathi Coming',
+        path: 'Song1.mp3',
+        artist: 'Anirudh Ravichander and Gana Balachandar',
+        cover: 'play1.jpg'
+        
+    },
+    {
+        name: 'Edhirthu-Nil',
+        path: 'Song2.mp3',
+        artist: 'Yuvan Shankar Raja, G.V. Prakash Kumar, D. Imman',
+        cover: 'play2.jpg'
+    },
+    {
+        name: 'Neruppu Da',
+        path: 'Song3.mp3',
+        artist: 'Arunraja Kamaraj and Santhosh Narayanan',
+        cover: 'play3.jpg'
+    },
+    {
+        name: 'Dammaku Dammaku',
+        path: 'Song4.mp3',
+        artist: 'Benny Dayal and Harris Jayaraj',
+        cover: 'play4.jpg'
+    },
+    {
+        name: 'Vaadi Pulla Vaadi',
+        path: 'Song5.mp3',
+        artist: 'Hiphop Tamizha',
+        cover: 'play5.jpg'
+    },
+    {
+        name: 'song 6',
+        path: 'assets/musics/Song 6.mp3',
+        artist: 'artist 6',
+        cover: 'play 6.jpg'
+    },
+    {
+        name: 'song 6',
+        path: 'assets/musics/Song 6.mp3',
+        artist: 'artist 6',
+        cover: 'play 6.jpg'
+    }
+]
